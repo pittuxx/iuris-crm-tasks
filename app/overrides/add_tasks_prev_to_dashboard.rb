c7e2a53a4fc2c92cf66e861a6160682deaf5a4ff
pittuxx <pittuxx@gmail.com>
2015-11-06 19:21:06 +0100

    datatables-tasks

--- app/overrides/add_tasks_prev_to_dashboard.rb
+++ app/overrides/add_tasks_prev_to_dashboard.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path => "iuris/dashboard/index",
 										:name => "add_tasks_prev_to_dashboard",
 										:insert_after => "[data-iuris-hook='dashboard']",
-										:partial => "overrides/tasks_prev",
+										:partial => "overrides/dashboard_tasks_prev",
 										:namespaced => true,
-										:original => '')
+										:original => '0ac0826c8309a563daa0223079e6895d1ab93ae3')
--- app/overrides/add_tasks_to_contact.rb
+++ app/overrides/add_tasks_to_contact.rb
@@ -3,7 +3,7 @@ if Iuris::Core.avaiable?(:contacts)
 	Deface::Override.new(:virtual_path => "iuris/contacts/contacts/show",
 											:name => "add_tasks_to_contact",
 											:insert_after => "[data-iuris-hook='contacts_show']",
-											:partial => "overrides/contact_tasks",
+											:partial => "overrides/contact_tasks_list",
 											:namespaced => true,
 											:original => 'bfcca4c1d323c098f7f62c8fc434a8d5f6ee1a92')
 
